7f825f930a6d526f5e03083003bdb60eb51a21c4
Daniel McNevin <dmcnevin@iMac-2.local>
2009-12-11 10:58:31 -0500

    finished some basic features

--- app/controllers/spoilers_controller.rb
+++ app/controllers/spoilers_controller.rb
@@ -8,9 +8,27 @@ class SpoilersController < ApplicationController
     @spoilers = @user.spoilers
   end
   
+  def create
+    @user.spoilers.create(params[:spoiler])
+    respond_to do |format|
+      format.html { redirect_to @user }
+    end
+  end
+  
+  def destroy
+    @spoiler = @user.spoilers.find(params[:id])
+    if @spoiler.destroy
+      flash[:notice] = "Tag Removed"
+    else
+      flash[:error] = "There was a problem removing the tag"
+    end
+    redirect_to @user
+  end
+  
   private
   
   def get_user
-    @user = User.find(params[:user_id])
+    # @user = User.find(params[:user_id])
+    @user = current_user
   end
 end
---
+++ app/models/redacted.rb
@@ -0,0 +1,10 @@
+class Redacted
+
+  attr_accessor :tweet, :tags
+  
+  def initialize(tweet,tags)
+    @tweet = tweet
+    @tags = tags
+  end
+  
+end
