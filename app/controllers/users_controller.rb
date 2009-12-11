3ab8ceeccfffa016aa5ec3b5bc1f6ac8176d1e57
Daniel McNevin <dmcnevin@iMac-2.local>
2009-12-11 10:58:31 -0500

    finished some basic features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by_login(params[:id])
-    @tweets = []
+    @user = User.find(params[:id])
   end
 
 end
--- app/models/spoiler.rb
+++ app/models/spoiler.rb
@@ -1,3 +1,3 @@
 class Spoiler < ActiveRecord::Base
-  belongs_to :user_id
+  belongs_to :user
 end
