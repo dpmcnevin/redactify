71b4e28829dd9c206d92e4b4f71b94971f187bc7
Daniel McNevin <dpmcnevin@gmail.com>
2009-12-12 19:42:11 -0500

    changed the add popular tag to be less efficient, made the css worse

--- app/models/spoiler.rb
+++ app/models/spoiler.rb
@@ -2,9 +2,14 @@ class Spoiler < ActiveRecord::Base
   belongs_to :user
   
   validates_uniqueness_of :name, :on => :create, :message => "must be unique", :scope => :user_id
-  validates_format_of :name, :with => /^[\w\d\s]+$/, :on => :create, :message => "is invalid", :allow_blank => false
+  validates_format_of :name, :with => /^.*$/, :on => :create, :message => "is invalid", :allow_blank => false
   
   def self.popular
-    all(:select => "spoilers.id, spoilers.name", :group => :name)
+    # Assuming your model is named Model and its table is models:
+    # cols = column_names.collect {|c| "#{c}"}.join(",")
+    # And this is something like your query
+    # find_by_sql("SELECT #{cols}, count(*) AS count GROUP BY name, #{cols}")
+    # all(:select => "spoilers.id, spoilers.name", :group => :name)
+    all.collect(&:name).sort.uniq
   end
 end
