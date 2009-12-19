bc0737a5a3f8b4dc9598a40de319d929b194bd2a
Daniel McNevin <dmcnevin@iMac-2.local>
2009-12-19 08:45:56 -0500

    added redacted model

---
+++ app/models/tweet.rb
@@ -0,0 +1,25 @@
+class Tweet
+  
+  attr_reader :tweet
+  
+  def initialize(tweet)
+    @tweet = tweet
+  end
+  
+  def id
+    @tweet["id"]
+  end
+  
+  def text
+    @tweet["text"]
+  end
+  
+  def screen_name
+    @tweet["user"]["screen_name"]
+  end
+  
+  def profile_image_url
+    @tweet["user"]["profile_image_url"]
+  end
+  
+end
