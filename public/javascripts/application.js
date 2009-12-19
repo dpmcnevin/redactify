6eb235d2a81e12b9168147a08385d796bb42e4d6
Daniel McNevin <dmcnevin@iMac-2.local>
2009-12-19 09:57:41 -0500

    updated styles

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+Event.observe(window, 'load', function() {
+  var fade=setTimeout("fadeout()",3500);
+  var hide=setTimeout("$('flash').hide()",4800);
+});
+
+function fadeout(){
+  new Effect.Opacity("flash", {duration:1.5, from:1.0, to:0.0});
+}   
