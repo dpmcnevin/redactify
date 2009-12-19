37423d2f15b9e73457d3d41e4d5858dc4bf934a2
Daniel McNevin <dmcnevin@iMac-2.local>
2009-12-19 14:44:16 -0500

    added analytics and linking of twitter names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def auto_link_twitter(txt)
+  	if match = txt.match(/.*?(@)((?:[a-z][a-z]+))(:|\s)/i)
+  		user = match[2]
+  		txt.gsub!(user, '<a href="http://twitter.com/' + user + '">' + user + '</a>')
+  	end
+  	txt
+  end
+
 end
