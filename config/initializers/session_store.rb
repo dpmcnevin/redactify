df6525df8fb7640e64ae9a2499d577f7b769a4a6
Daniel McNevin <dmcnevin@iMac-2.local>
2009-12-19 13:01:33 -0500

    changed app name to redactify

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_spoiler-free_session',
+  :key         => '_redactify_session',
   :secret      => '0952cd42100c44df4c2554f9aaa3bce5f17b16e4b96d2f340481b3bce5c1274931bb6dfea5cecfa223e5ba3aa059f28609937edb77ddc93a71a3f78111bb8228'
 }
 
