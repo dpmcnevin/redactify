a030f0032d1b12af6ae58430190ce2e793e07b1f
Daniel McNevin <dpmcnevin@gmail.com>
2009-12-19 19:33:02 -0500

    made it a closed alpha

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
   
   before_filter :check_login
+  before_filter :check_authorized_users
   
   def show
     # @user = User.find(params[:id])
     @user = current_user
     @rate_limit = @user.rate_limit_status
+  end
+  
+  private
+  
+  def check_authorized_users
+    users = ["mental_thinking", "redactify", "spoiler_free", "ignignokt_"]
+    unless users.include? current_user.login
+      flash[:error] = "This is a closed test system for now, check back later for beta signups"
+      redirect_to root_path
+    end
   end
 
 end
