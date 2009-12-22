ad14b24fed44032195b5c48259b220c3a59b1313
Daniel McNevin <dpmcnevin@gmail.com>
2009-12-21 20:34:42 -0500

    more css updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@
 module ApplicationHelper
 
   def auto_link_twitter(txt)
-  	txt.scan(/(^|\s+)(#|@)(\w{1,15})/).each do |match|
+  	txt.scan(/(^|\W|\s+)(#|@)(\w{1,15})/).each do |match|
   	  if match[1] == "#"
   	    txt.gsub!(match.last, link_to(match.last, "http://twitter.com/search/?q=##{match.last}", :target => "_blank"))
 	    elsif match[1] == "@"
-	      txt.gsub!(match.last, link_to(match.last, "http://twitter.com/match.last", :target => "_blank"))
+	      txt.gsub!(match.last, link_to(match.last, "http://twitter.com/#{match.last}", :target => "_blank"))
 		  end
   	end
   	txt
