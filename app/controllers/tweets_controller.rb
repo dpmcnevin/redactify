5bb80c1dedded77f5774217ed232917af9d5d0b8
Daniel McNevin <dpmcnevin@gmail.com>
2009-12-27 22:40:35 -0500

    more css updates, rearranged front page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,8 @@
 class TweetsController < ApplicationController
   
   def show
-    @tweet = current_user.get_tweet(params[:id])
+    @user = current_user
+    @tweet = @user.get_tweet(params[:id])
   end
 
 end
