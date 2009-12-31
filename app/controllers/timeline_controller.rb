bc256bae39e08e8c217cb725f7b6aa62dc1da927
Daniel McNevin <dmcnevin@iMac-2.local>
2009-12-31 18:35:45 -0500

    added json

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -5,6 +5,10 @@ class TimelineController < ApplicationController
     @user = current_user
     @page = params[:page].to_i + 1 if params[:page]
     @timeline = @user.spoiler_free_timeline(params)
+    respond_to do |format|
+      format.html
+      format.json { render :json => @timeline.to_json }
+    end
   end
 
 end
