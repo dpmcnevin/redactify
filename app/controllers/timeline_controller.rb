441525e19a287bc4868a7702e4112c1284a00170
Daniel McNevin <dpmcnevin@gmail.com>
2009-12-31 09:32:49 -0500

    removed user restrictions

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,20 +1,10 @@
 class TimelineController < ApplicationController
   before_filter :check_login
-  before_filter :check_authorized_users
   
   def index
     @user = current_user
     @page = params[:page].to_i + 1 if params[:page]
     @timeline = @user.spoiler_free_timeline(params)
   end
-  
-  private
-  
-  def check_authorized_users
-    users = ["mental_thinking", "redactify", "spoiler_free", "ignignokt_"]
-    unless users.include? current_user.login
-      flash[:error] = "This is a closed test system for now, check back later for beta signups"
-      redirect_to root_path
-    end
-  end
+
 end
