f84f748495781e3c9f275332e4e0706e313b83c1
Daniel McNevin <dpmcnevin@gmail.com>
2009-12-31 23:58:40 -0500

    added a loading update when getting more entries

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,6 +8,11 @@ Event.observe(window, 'load', function() {
   }
 });
 
-function fadeout(){
+function fadeout() {
   new Effect.Opacity("flash", {duration:1.5, from:1.0, to:0.0});
 }   
+
+function loading(section) {
+  $(section).update("loading...")
+}
+
