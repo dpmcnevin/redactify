c13532e8aa59f36111509e62d24a7ce0a77e6b99
Daniel McNevin <dpmcnevin@gmail.com>
2010-01-09 22:44:37 -0500

    got the autoupdate working right and the auto_link_twitter not messing things up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@
 module ApplicationHelper
 
   def auto_link_twitter(txt)
-  	txt.scan(/(^|\W|\s+)(#|@)(\w{1,15})/).each do |match|
+  	txt.scan(/(^|\W|\s+)(#|@)(\w{1,25})/).each do |match|
   	  if match[1] == "#"
-  	    txt.gsub!(match.last, link_to(match.last, "http://twitter.com/search/?q=##{match.last}", :target => "_blank"))
+  	    txt.gsub!(/##{match.last}/, link_to("##{match.last}", "http://twitter.com/search/?q=##{match.last}", :target => "_blank"))
 	    elsif match[1] == "@"
-	      txt.gsub!(match.last, link_to(match.last, "http://twitter.com/#{match.last}", :target => "_blank"))
+	      txt.gsub!(/@#{match.last}/, link_to("@#{match.last}", "http://twitter.com/#{match.last}", :target => "_blank"))
 		  end
   	end
   	txt
@@ -15,7 +15,6 @@ module ApplicationHelper
   def update_rate_limit
     update_page do |page|
       page["rate_limit"].replace_html :partial => "users/rate_limit", :locals => { :rate_limit => @user.rate_limit_status }
-      page["rate_limit"].highlight
     end
   end
   
