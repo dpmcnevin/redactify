059f93ea1d0f99206dc9e3f7613b4458bc9e9c6c
Daniel McNevin <dpmcnevin@gmail.com>
2010-01-09 23:47:42 -0500

    changed the auto_link_twitter to take options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-  def auto_link_twitter(txt)
+  def auto_link_twitter(txt, options = {:target => "_blank"})
   	txt.scan(/(^|\W|\s+)(#|@)(\w{1,25})/).each do |match|
   	  if match[1] == "#"
-  	    txt.gsub!(/##{match.last}/, link_to("##{match.last}", "http://twitter.com/search/?q=##{match.last}", :target => "_blank"))
+  	    txt.gsub!(/##{match.last}/, link_to("##{match.last}", "http://twitter.com/search/?q=##{match.last}", options))
 	    elsif match[1] == "@"
-	      txt.gsub!(/@#{match.last}/, link_to("@#{match.last}", "http://twitter.com/#{match.last}", :target => "_blank"))
+	      txt.gsub!(/@#{match.last}/, link_to("@#{match.last}", "http://twitter.com/#{match.last}", options))
 		  end
   	end
   	txt
