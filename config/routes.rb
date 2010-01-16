f3a48fa86ef37ceccf2d88ca98d3724f9ddea3be
Daniel McNevin <dpmcnevin@gmail.com>
2010-01-16 13:22:32 -0500

    got lists and users working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   
-  map.resources :users, :only => [:show, :new], :has_many => :spoilers
+  map.resources :users, :only => [:show, :update], :has_many => :spoilers
   map.resources :tweets, :only => [:show]
   map.resource :timeline, :only => [:show, :update, :create]
   map.resources :lists, :only => [:show, :update]
