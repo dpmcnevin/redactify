64445f80f64225273993c0f76b169081ea1cb0c5
Daniel McNevin <dpmcnevin@gmail.com>
2010-01-16 15:13:15 -0500

    added search

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,17 @@
+class SearchesController < ApplicationController
+
+  before_filter :check_login
+  before_filter :load_user
+  after_filter :update_latest_tweet, :only => :create
+  
+  def create
+    @timeline = @user.spoiler_free_timeline(:search => true, :q => params[:q])
+    render "timelines/show"
+  end
+  
+  def update
+    @new_tweets = @user.spoiler_free_timeline(:screen_name => params[:id], :since_id => session[:latest_id])
+    load_new_tweets(@new_tweets)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :tweets, :only => [:show]
   map.resource :timeline, :only => [:show, :update, :create]
   map.resources :lists, :only => [:show, :update]
-  
-  # map.timeline "/timeline", :controller => "timeline", :action => :index
+  map.resources :searches, :only => [:create]
 
-  
   map.static '/:action', :controller => 'static'
 
-  # map.root :controller => 'static', :action => 'index'
   map.root :timeline
 end
