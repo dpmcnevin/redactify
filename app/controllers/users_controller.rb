a1e6ebff41df99f5a40f7a233ccfd738750a949f
Daniel McNevin <dpmcnevin@gmail.com>
2010-01-28 23:29:05 -0500

    forgot trends on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   after_filter :update_latest_tweet, :only => :show
   
   def show
+    @trends = @user.trends
     @page = params[:page] ||= 1
     @get_more_url = user_path(:id => params[:id], :page => @page.to_i + 1)
     @update_tweets_url = user_path(:id => params[:id])
