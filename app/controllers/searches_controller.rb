a39951a7077f108055c7028568c53359e05b3641
Daniel McNevin <dpmcnevin@gmail.com>
2010-01-28 22:35:54 -0500

    broke out a trends controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,6 +5,7 @@ class SearchesController < ApplicationController
   after_filter :update_latest_tweet, :only => :create
   
   def create
+    @trends = @user.trends
     @timeline = @user.spoiler_free_timeline(:search => true, :q => params[:q])
     render "timelines/show"
   end
