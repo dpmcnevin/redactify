d91ee2007b8b5359978204b63a4fc2ecb6d371cb
Daniel McNevin <dpmcnevin@gmail.com>
2010-07-28 21:20:06 -0400

    allowing net connections for now for the cucumber selenium tests

--- features/support/fakeweb_helpers.rb
+++ features/support/fakeweb_helpers.rb
@@ -1,4 +1,4 @@
-FakeWeb.allow_net_connect = false
+# FakeWeb.allow_net_connect = false
 FakeWeb.register_uri(:post, 'http://twitter.com/oauth/request_token', :body => 'oauth_token=fake&oauth_token_secret=fake')
 FakeWeb.register_uri(:post, 'http://twitter.com/oauth/access_token', :body => 'oauth_token=fake&oauth_token_secret=fake')
 FakeWeb.register_uri(:get, 'http://twitter.com/account/verify_credentials.json', :response => File.join(Rails.root, "features", "twitter_response", 'verify_credentials.json'))
